17fe1fba7ad2c70dcd6865a4e4c78c4ffda0b163
Gerald Kim Ico <gerald_ico@launchlabs.ph>
2016-08-02 14:10:42 +0800

    ver 1.0

--- inAppPurchase/inAppPurchase/ViewController.h
+++ inAppPurchase/inAppPurchase/ViewController.h
@@ -7,9 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <StoreKit/StoreKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController<SKProductsRequestDelegate,SKPaymentTransactionObserver> {
+    SKProductsRequest *productsRequest;
+    NSArray *validProducts;
+    UIActivityIndicatorView *activityIndicatorView;
+    IBOutlet UILabel *productTitleLabel;
+    IBOutlet UILabel *productDescriptionLabel;
+    IBOutlet UILabel *productPriceLabel;
+    IBOutlet UIButton *purchaseButton;
+}
+- (void)fetchAvailableProducts;
+- (BOOL)canMakePurchases;
+- (void)purchaseMyProduct:(SKProduct*)product;
+- (IBAction)purchase:(id)sender;
 
 @end
 
--- inAppPurchase/inAppPurchase/ViewController.m
+++ inAppPurchase/inAppPurchase/ViewController.m
@@ -16,12 +16,98 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // Adding activity indicator
+    activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    activityIndicatorView.center = self.view.center;
+    [activityIndicatorView hidesWhenStopped];
+    [self.view addSubview:activityIndicatorView];
+    [activityIndicatorView startAnimating];
+    
+    //Hide purchase button initially
+    purchaseButton.hidden = YES;
+    [self fetchAvailableProducts];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)fetchAvailableProducts {
+    NSSet *productIdentifiers = [NSSet setWithObjects:kTutorialPointProductID,nil];
+    productsRequest = [[SKProductsRequest alloc] initWithProductIdentifiers:productIdentifiers];
+    productsRequest.delegate = self;
+    [productsRequest start];
+}
+
+- (BOOL)canMakePurchases {
+    return [SKPaymentQueue canMakePayments];
+}
+
+- (void)purchaseMyProduct:(SKProduct*)product{
+    if ([self canMakePurchases]) {
+    SKPayment *payment = [SKPayment paymentWithProduct:product];
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+    [[SKPaymentQueue defaultQueue] addPayment:payment];
+    } else {
+        UIAlertView *alertView = [[UIAlertView alloc]initWithTitle: @"Purchases are disabled in your device" message:nil delegate:self cancelButtonTitle:@"Ok" otherButtonTitles: nil];
+        [alertView show];
+    }
+}
+
+-(IBAction)purchase:(id)sender {
+    [self purchaseMyProduct:[validProducts objectAtIndex:0]];
+    purchaseButton.enabled = NO;
+}
+
+#pragma mark StoreKit Delegate
+
+-(void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transactions {
+    for (SKPaymentTransaction *transaction in transactions) {
+        switch (transaction.transactionState) {
+            case SKPaymentTransactionStatePurchasing: NSLog(@"Purchasing");
+            break;
+            case SKPaymentTransactionStatePurchased:
+                if ([transaction.payment.productIdentifier
+                 isEqualToString:kTutorialPointProductID]) {
+                    NSLog(@"Purchased ");
+                    UIAlertView *alertView = [[UIAlertView alloc]initWithTitle: @"Purchase is completed succesfully" message:nil delegate: self cancelButtonTitle:@"Ok" otherButtonTitles: nil];
+                    [alertView show];
+                }
+                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateRestored: NSLog(@"Restored ");
+                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateFailed: NSLog(@"Purchase failed ");
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+-(void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response
+{
+    SKProduct *validProduct = nil;
+    int count = (int)[response.products count];
+    if (count>0) {
+        validProducts = response.products;
+        validProduct = [response.products objectAtIndex:0];
+        if ([validProduct.productIdentifier isEqualToString:kTutorialPointProductID]) {
+            [productTitleLabel setText:[NSString stringWithFormat: @"Product Title: %@",validProduct.localizedTitle]];
+            [productDescriptionLabel setText:[NSString stringWithFormat: @"Product Desc: %@",validProduct.localizedDescription]];
+            [productPriceLabel setText:[NSString stringWithFormat: @"Product Price: %@",validProduct.price]];
+        }
+    } else {
+        UIAlertView *tmp = [[UIAlertView alloc] initWithTitle:@"Not Available" message:@"No products to purchase" delegate:self cancelButtonTitle:nil otherButtonTitles:@"Ok", nil];
+        [tmp show];
+    }
+    [activityIndicatorView stopAnimating];
+    purchaseButton.hidden = NO;
 }
 
 @end
